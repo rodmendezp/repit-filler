3293c1dad876eecc534f665f7ae205cf917f17bb
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-24 16:14:45 -0300

    added status model

--- filler/models.py
+++ filler/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Status(models.Model):
+    processing = models.BooleanField()
+    locked = models.BooleanField()
+    jobs_available = models.BooleanField()
