51fcaea5c659cff27c849667ebddbadedf29c68c
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-27 22:17:39 -0300

    added games and streamers views for options

--- filler/urls.py
+++ filler/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     url(r'^request_jobs/$', views.RequestJobs.as_view()),
     url(r'^cancel_jobs/$', views.CancelJobs.as_view()),
     url(r'^fake_jobs/$', views.FakeJobs.as_view()),
-    url(r'^clear_jobs/$', views.ClearJobs.as_view())
+    url(r'^clear_jobs/$', views.ClearJobs.as_view()),
+    url(r'^games/$', views.FillerGameList.as_view()),
+    url(r'^streamers/$', views.FillerStreamerList.as_view())
 ]
