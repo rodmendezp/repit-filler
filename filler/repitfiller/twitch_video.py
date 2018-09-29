fede554fa9890e9da61e2d241042a6610f67c129
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-29 13:59:22 -0300

    post video to repit data and to own video table and check own

--- filler/repitfiller/twitch_video.py
+++ filler/repitfiller/twitch_video.py
@@ -1,6 +1,9 @@
 import os
 import json
+from datetime import time
 from repitapi.client import RepitClient
+from filler.models import Video
+from django.core.serializers.json import DjangoJSONEncoder
 from .repit_twitch_api import RepitTwitchAPI
 
 
@@ -12,25 +15,58 @@ class TwitchVideo:
         with open(settings_path, 'r') as f:
             self.settings = json.load(f)
         self.twitch_client = RepitTwitchAPI()
+        self.game = None
         self.videos = []
         self.twitch_api_limit = 99
 
-    def get_new_video_id(self):
-        video_id = None
-        while video_id is None:
-            if self.videos:
-                video_id = self.videos.pop(0)['id'].replace('v', '')
-                while self.video_already_exist(video_id) and self.videos:
-                    video_id = self.videos.pop(0)['id'].replace('v', '')
-            if not video_id:
+    def get_new_video(self):
+        video = None
+        while video is None:
+            if not self.videos:
                 self.get_new_videos()
-        return video_id
+            while self.videos:
+                check_video = self.videos.pop(0)
+                if not self.past_video(check_video['id'].replace('v', '')):
+                    video = check_video
+                    break
+        Video.objects.create(twid=video['id'].replace('v', ''))
+        return video
 
-    def video_already_exist(self, video_id):
-        return self.repit_twitch_client.video.get_objects({'twid': video_id}) != []
+    @staticmethod
+    def past_video(video_id):
+        try:
+            Video.objects.get(twid=video_id)
+            return True
+        except Video.DoesNotExist:
+            return False
+
+    @staticmethod
+    def seconds_to_h_m_s(secs):
+        h = int(secs / 3600)
+        secs -= h * 3600
+        m = int(secs / 60)
+        secs -= m * 60
+        return {
+            'hour': h,
+            'minute': m,
+            'second': secs
+        }
+
+    def post_video_repit_data(self, video):
+        data = {
+            'twid': video['id'].replace('v', ''),
+            'streamer_twid': video['channel']['id'],
+            'streamer_name': video['channel']['name'],
+            'game_twid': self.game['id'],
+            'game_name': video['game'],
+            'recorded': json.dumps(video['created_at'], cls=DjangoJSONEncoder),
+            'length': json.dumps(time(**self.seconds_to_h_m_s(video['length'])), cls=DjangoJSONEncoder)
+        }
+        return self.repit_twitch_client.video.post_object(data)
 
     # TODO: Add a way to not check all videos again
     def get_new_videos(self):
+        self.game = self.get_twitch_game(self.settings['video']['game'])
         for streamer in self.settings['video']['streamer_priorities']:
             offset = 0
             params = {
@@ -56,3 +92,10 @@ class TwitchVideo:
                 videos = self.twitch_client.get_streamer_videos(params)
                 videos = list(filter(lambda x: x['game'] == self.settings['video']['game'], videos))
         return None
+
+    def get_twitch_game(self, game_name):
+        games = self.twitch_client.client.games.get_top(limit=100)
+        games = list(filter(lambda x: x['game']['name'] == game_name, games))
+        if len(games) == 0:
+            return None
+        return games[0]['game']
