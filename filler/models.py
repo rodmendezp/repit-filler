b59392bc32828e345469867162b5886734b0d01e
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-29 13:51:06 -0300

    added vidoe and candidate to keep track of what has been checked already

--- filler/models.py
+++ filler/models.py
@@ -24,3 +24,17 @@ class StreamerGame(models.Model):
     id = models.AutoField(db_column='ID', primary_key=True)
     streamer = models.ForeignKey(FillerStreamer, on_delete=models.DO_NOTHING)
     game = models.ForeignKey(FillerGame, on_delete=models.DO_NOTHING)
+
+
+class Video(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)
+    twid = models.IntegerField(unique=True)
+    candidates = models.BooleanField(default=False)
+
+
+class Candidate(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)
+    video = models.ForeignKey(Video, on_delete=models.DO_NOTHING)
+    start = models.TimeField()
+    end = models.TimeField()
+    ack = models.BooleanField(default=False)
