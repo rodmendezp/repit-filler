2fc5fc3cbefd69edacb4d75eba6d5452e91230a4
Rodrigo Mendez <rodmendezp@gmail.com>
2018-09-29 13:54:40 -0300

    add video and candidate serializer

--- filler/serializers.py
+++ filler/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from filler.models import FillerGame, FillerStreamer
+from filler.models import *
 
 
 class FillerGameSerializer(serializers.ModelSerializer):
@@ -12,3 +12,17 @@ class FillerStreamerSerializer(serializers.ModelSerializer):
     class Meta:
         model = FillerStreamer
         fields = ('name', )
+
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = ('twid', 'candidates')
+
+
+class CandidateSerializer(serializers.ModelSerializer):
+    video = VideoSerializer
+
+    class Meta:
+        model = Candidate
+        fields = ('video', 'start', 'end', 'ack')
