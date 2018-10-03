8381c7fb2523addaec781f2a58cc5e5b9fde06e6
Rodrigo Mendez <rodmendezp@gmail.com>
2018-10-03 11:25:56 -0300

    add check for status table and status row if not exist create it

--- filler/apps.py
+++ filler/apps.py
@@ -1,22 +1,30 @@
 from django.apps import AppConfig
+from django.db import connection
 
 
 class FillerConfig(AppConfig):
     name = 'filler'
 
+    @staticmethod
+    def db_table_exists(table_name):
+        return table_name in connection.introspection.table_names()
+
     def ready(self):
-        self.reset_status()
+        if self.db_table_exists('filler_status'):
+            self.reset_status()
         super().ready()
 
     @staticmethod
     def reset_status():
         from filler.repitfiller.repit_filler import RepitJobQueue
         from .models import Status
-        status = Status.objects.get(pk=1)
+        try:
+            status = Status.objects.get(pk=1)
+        except Status.DoesNotExist:
+            status = Status.objects.create(processing=False, locked=False, jobs_available=False)
         repit_job_queue = RepitJobQueue()
         status.jobs_available = repit_job_queue.jobs_available()
         status.locked = False
         status.processing = False
         status.save()
         repit_job_queue.close_connection()
-
