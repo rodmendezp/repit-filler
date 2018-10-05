bd19b05b1865ac30b601cf71338c81cdcaad3572
Rodrigo Mendez <rodmendezp@gmail.com>
2018-10-05 10:38:08 -0300

    added loaddata

--- resetDBMigrations.bat
+++ resetDBMigrations.bat
@@ -3,3 +3,8 @@ find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc"  -delete
 python manage.py makemigrations
 python manage.py migrate
+find . -path "*/fixtures/*.json" > fixtures.json
+FOR /F %%i IN (.\fixtures.json) DO (
+  python manage.py loaddata %%i
+)
+rm fixtures.json
