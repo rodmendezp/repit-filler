e5aa0e8374b7f412bf3d63d0db9e3a1175985da8
Rodrigo Mendez <rodmendezp@gmail.com>
2018-10-05 10:12:23 -0300

    init repit_filler when starting app

--- filler/apps.py
+++ filler/apps.py
@@ -5,6 +5,10 @@ from django.db import connection
 class FillerConfig(AppConfig):
     name = 'filler'
 
+    def __init__(self, app_name, app_module):
+        self.repit_filler = None
+        super().__init__(app_name, app_module)
+
     @staticmethod
     def db_table_exists(table_name):
         return table_name in connection.introspection.table_names()
@@ -12,6 +16,7 @@ class FillerConfig(AppConfig):
     def ready(self):
         if self.db_table_exists('filler_status'):
             self.reset_status()
+        self.init_repit_filler()
         super().ready()
 
     @staticmethod
@@ -28,3 +33,8 @@ class FillerConfig(AppConfig):
         status.processing = False
         status.save()
         repit_job_queue.close_connection()
+
+    def init_repit_filler(self):
+        from filler.repitfiller.repit_filler import RepitFiller
+        self.repit_filler = RepitFiller()
+        self.repit_filler.init_jobs()
