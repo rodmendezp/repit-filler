9832b62c65f4f684162928e8bf687c0180d304e0
Rodrigo Mendez <rodmendezp@gmail.com>
2018-10-09 18:11:18 -0300

    added test to get queues

--- filler/tests.py
+++ filler/tests.py
@@ -1,6 +1,7 @@
 import requests
 from django.test import TestCase
 from filler.repitfiller.repit_filler import RepitFiller
+from filler.repitfiller.rabbitmq_api.client import RabbitMQClient
 
 
 class RepitFillerTest(TestCase):
@@ -10,12 +11,9 @@ class RepitFillerTest(TestCase):
     def test_repit_filler_creation(self):
         repit_filler = RepitFiller()
         repit_filler.init_jobs()
-        repit_filler.init_request()
         self.assertEqual(repit_filler.connection.is_open, True)
         self.assertEqual(repit_filler.jobs_queue.channel_number, repit_filler.jobs_channel_id)
         self.assertEqual(repit_filler.jobs_queue.method.queue, repit_filler.jobs_queue_name)
-        self.assertEqual(repit_filler.request_queue.channel_number, repit_filler.request_channel_id)
-        self.assertEqual(repit_filler.request_queue.method.queue, repit_filler.request_queue_name)
 
 
 class RepitFillerFillTest(TestCase):
@@ -40,3 +38,17 @@ class RepitAPITest(TestCase):
         response = requests.get(twitch_user_url)
         print(response)
 
+
+class RabbitMQAPITest(TestCase):
+    def setUp(self):
+        super().setUp()
+
+    def test_get_queues(self):
+        self.rabbitmq_client = RabbitMQClient()
+        queues = self.rabbitmq_client.queue.get_queues()
+        queues_names = list(map(lambda x: x['name'], queues))
+        bindings = []
+        for queue_name in queues_names:
+            bindings.append(self.rabbitmq_client.queue.get_queue_bindings(queue_name))
+        print(bindings)
+        pass
