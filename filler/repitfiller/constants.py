dd9583908a7e968093c653ac35deb3281eb1ab39
Rodrigo Mendez <rodmendezp@gmail.com>
2018-10-09 18:05:40 -0300

    added repitfiller constants

---
+++ filler/repitfiller/constants.py
@@ -0,0 +1,8 @@
+EXCHANGE_NAME = 'repit_tasks'
+RABBIT_HOST = 'localhost'
+TASKS_GAME = 100
+TASKS_CUSTOM = 20
+REPITIT_FILLER_HOST = 'http://127.0.0.1:9999'
+FILLER_API = '%s/filler' % REPITIT_FILLER_HOST
+GAME_QUEUE_ENDPOINT = '%s/game_queue_status/' % FILLER_API
+CUSTOM_QUEUE_ENDPOINT = '%s/custom_queue_status/' % FILLER_API
