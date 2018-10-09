3e4c2b8e654d3304fc9067f5b4a3936d77c5f7e2
Rodrigo Mendez <rodmendezp@gmail.com>
2018-10-09 18:12:09 -0300

    get repit_filler from app config instead of instantiating it again and use game queue status

--- filler/tasks.py
+++ filler/tasks.py
@@ -1,22 +1,34 @@
 from __future__ import absolute_import
+from django.apps import apps
 from celery import shared_task
-from .models import Status
+from .models import GameQueueStatus, CustomQueueStatus
 from .repitfiller.repit_filler import RepitFiller
 
 
 @shared_task
-def request_jobs():
-    repit_filler = RepitFiller()
-    repit_filler.init_jobs()
-    status = Status.objects.get(pk=1)
-    if status.processing:
+def request_jobs(game, streamer, user):
+    repit_filler = apps.get_app_config('filler').repit_filler
+    if user:
+        filler_queue_status = CustomQueueStatus.objects.filter(game=game, streamer=streamer, user=user)
+    else:
+        filler_queue_status = GameQueueStatus.objects.filter(game=game)
+    filler_queue_status = filler_queue_status[0] if len(filler_queue_status) == 1 else None
+    if filler_queue_status.processing:
         return
-    status.locked = False
-    status.processing = True
-    status.save()
-    repit_filler.add_jobs(10)
-    status = Status.objects.get(pk=1)
-    if status.locked:
+    filler_queue_status.locked = False
+    filler_queue_status.processing = True
+    filler_queue_status.save()
+    if user:
+        repit_filler.add_tasks_custom(game, user, streamer)
+    else:
+        repit_filler.add_tasks_game(game)
+    # repit_filler.close_connection()
+    if user:
+        filler_queue_status = CustomQueueStatus.objects.filter(game=game, streamer=streamer, user=user)
+    else:
+        filler_queue_status = GameQueueStatus.objects.filter(game=game)
+    filler_queue_status = filler_queue_status[0] if len(filler_queue_status) == 1 else None
+    if filler_queue_status.locked:
         return
-    status.processing = False
-    status.save()
+    filler_queue_status.processing = False
+    filler_queue_status.save()
