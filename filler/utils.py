4825711126c34697c5a3ca250d5952cdc542205b
Rodrigo Mendez <rodmendezp@gmail.com>
2018-10-27 13:56:51 -0300

    moved common functions to utils

---
+++ filler/utils.py
@@ -0,0 +1,50 @@
+from django.apps import apps
+from .repitfiller.repit_filler import RepitTaskQueue
+from .models import GameQueueStatus, CustomQueueStatus
+
+
+def get_request_params(params):
+    game = params.get('game', None)
+    streamer = params.get('streamer', '')
+    user = params.get('user', '')
+    return game, streamer, user
+
+
+def get_queue_status(game, streamer, user):
+    if user:
+        filler_queue_status = CustomQueueStatus.objects.filter(game=game, streamer=streamer, user=user)
+    else:
+        filler_queue_status = GameQueueStatus.objects.filter(game=game)
+    return filler_queue_status[0] if len(filler_queue_status) == 1 else None
+
+
+def create_queue_status(game, streamer, user):
+    if user:
+        filler_queue_status = CustomQueueStatus.objects.create(game=game, streamer=streamer, user=user)
+    else:
+        filler_queue_status = GameQueueStatus.objects.create(game=game)
+    return filler_queue_status
+
+
+def get_or_create_queue_status(game, streamer, user):
+    filler_queue_status = get_queue_status(game, streamer, user)
+    if not filler_queue_status:
+        filler_queue_status = create_queue_status(game, streamer, user)
+    return filler_queue_status
+
+
+def get_task(queue_status):
+    repit_task_queue = RepitTaskQueue()
+    message_count = repit_task_queue.get_message_count(queue_status.queue_name)
+    if message_count == 1:
+        queue_status.jobs_available = False
+        queue_status.save()
+    repit_filler = apps.get_app_config('filler').repit_filler
+    return repit_filler.get_task_queue(queue_status.queue_name)
+
+
+def ack_task(delivery_tag):
+    repit_filler = apps.get_app_config('filler').repit_filler
+    response = repit_filler.ack_task(int(delivery_tag))
+    return response
+
