9eb60fee3c48ab7e22b40e0d806d46721f9a0592
Rodrigo Mendez <rodmendezp@gmail.com>
2018-11-04 21:30:41 -0300

    removed get taks from util and put it in repit filler

--- filler/utils.py
+++ filler/utils.py
@@ -1,5 +1,4 @@
 from django.apps import apps
-from .repitfiller.repit_filler import RepitTaskQueue
 from .models import GameQueueStatus, CustomQueueStatus
 
 
@@ -31,25 +30,6 @@ def get_or_create_queue_status(game, streamer, user):
     if not filler_queue_status:
         filler_queue_status = create_queue_status(game, streamer, user)
     return filler_queue_status
-
-
-def get_task(queue_status):
-    print('Getting Task')
-    repit_task_queue = RepitTaskQueue()
-    message_count = repit_task_queue.get_message_count(queue_status.queue_name)
-    if message_count == 1:
-        queue_status.jobs_available = False
-        queue_status.save()
-    print('Getting repit_filler')
-    repit_filler = apps.get_app_config('filler').repit_filler
-    print('Repit_Filler = ', repit_filler)
-    try:
-        print('queue name = %s' % queue_status.queue_name)
-        task = repit_filler.get_task_queue(queue_status.queue_name)
-    except Exception as e:
-        print('In get_task Exception')
-        print(e)
-    return task if task else None
 
 
 def ack_task(delivery_tag):
